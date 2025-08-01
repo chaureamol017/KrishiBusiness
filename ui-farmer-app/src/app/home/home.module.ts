7108a72b6add73b6f756e5e6730ba13e1d9d3305
chaureamol017 <chaureamol017@gmail.com>
2025-08-01 14:17:04 +0530

    Update home component

--- ui-farmer-app/src/app/home/home.module.ts
+++ ui-farmer-app/src/app/home/home.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { FlexLayoutModule } from '@angular/flex-layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { HomeComponent } from './home/home.component';
-import { MatIconModule, MatToolbarModule, MatButtonModule, MatMenuModule, MatDividerModule, MatSidenavModule } from '@angular/material';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { BannerComponent } from './banner/banner.component';
 import { CardBannerComponent } from './card-banner/card-banner.component';
+import { MaterialModule } from '../material/material.module';
 
-
-// const routes: Routes = [{
-//     path: 'login',
-//     component: LoginComponent
-//   }];
 
 @NgModule({
   declarations: [
@@ -22,20 +17,13 @@ import { CardBannerComponent } from './card-banner/card-banner.component';
     CardBannerComponent,
   ],
   imports: [
-    // RouterModule.forRoot(routes, { useHash: true }),
-    CommonModule,
-    
+    CommonModule,    
     FormsModule,
     ReactiveFormsModule,
 
-    // FlexLayoutModule,
+    MaterialModule,
 
-    MatButtonModule,
-    MatDividerModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatSidenavModule,
-
+    FlexLayoutModule,
   ]
 })
 export class HomeModule { }
