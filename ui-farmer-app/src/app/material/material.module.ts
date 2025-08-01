59aa859e7d4dff1fd56f4fc3498c6bfbe92f6ee9
chaureamol017 <chaureamol017@gmail.com>
2025-08-01 14:05:07 +0530

    Angular material module

---
+++ ui-farmer-app/src/app/material/material.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatListModule } from '@angular/material/list'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatDialogModule } from '@angular/material/dialog'
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatDialogModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+  ],
+  exports: [
+
+        MatButtonModule,
+        MatDividerModule,
+        MatDialogModule,
+        MatIconModule,
+        MatListModule,
+        MatMenuModule,
+        MatSidenavModule,
+        MatToolbarModule,
+    
+  ]
+})
+export class MaterialModule {
+}
