e131048de16792454cd37f886ff43c783de91430
chaureamol017 <chaureamol017@gmail.com>
2025-08-01 14:16:11 +0530

    added product module

--- ui-farmer-app/src/app/product/product.module.ts
+++ ui-farmer-app/src/app/product/product.module.ts
@@ -1,41 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductListComponent } from './product-list/product-list.component';
-import {
-  MatTableModule, MatIconModule, MatPaginatorModule, MatButtonModule, MatDialogModule, MatToolbarModule, MatDatepickerModule
-} from '@angular/material';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { SellingProductsListComponent } from './selling-products-list/selling-products-list.component';
-import { TopBarComponent } from '../shared-components/top-bar/top-bar.component';
-import { SharedComponentsModule } from '../shared-components/shared-components.module';
+import { ProductComponent } from './product/product.component';
 
 
 
 @NgModule({
   declarations: [
-    ProductListComponent,
-    SellingProductsListComponent,
-
-    
+    ProductComponent
   ],
   imports: [
-    CommonModule,
-    
-    FormsModule,
-    ReactiveFormsModule,
-    
-    MatButtonModule,
-    MatDatepickerModule,
-    MatDialogModule,
-    MatIconModule,
-    MatPaginatorModule,
-    MatTableModule,
-    MatToolbarModule,
-
-    SharedComponentsModule,
-  ],
-  exports: [
-    ProductListComponent,
+    CommonModule
   ]
 })
 export class ProductModule { }
