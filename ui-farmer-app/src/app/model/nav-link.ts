f59570525acaeaac61d9d293c6c95e5afcb6e092
chaureamol017 <chaureamol017@gmail.com>
2025-08-07 20:55:46 +0530

    Removed unused project

--- ui-farmer-app/src/app/model/nav-link.ts
+++
@@ -1,7 +0,0 @@
-
-export interface NavLink {
-    title: string;
-    path: string;
-    icon?: string;
-    role?: string;
-}
--- ui-farmer-app/src/app/model/product.ts
+++
@@ -1,6 +0,0 @@
-export interface Product {
-    product_id: number;
-    product_name: String;
-    product_description: String;
-    product_grade: String;
-}
