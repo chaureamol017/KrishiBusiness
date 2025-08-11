094bb8aec7cbe6be130aab685ea16883d19ccfb3
chaureamol017 <chaureamol017@gmail.com>
2025-08-11 13:53:36 +0530

    home component description style change

--- farmerapp/src/app/home/home/home.component.ts
+++ farmerapp/src/app/home/home/home.component.ts
@@ -11,15 +11,6 @@ export class HomeComponent implements OnInit {
 
   appName: any = "Krishi Business";
 
-  heading: any = "Welcome to KRUSHI BUSINESS";
-  description: any = "\"KRUSHI BUSINESS\" application help to perform online selling and buying of farm goods between farmer, retailer and buyer. "
-    + "Application help to create virtual market place for connecting farmer directly with retailers and wholesalers."
-    + "The farmer and buyer can create their profile online where they can update information related with the post of products(like quantity, price, bidding price etc.)."
-    + "The result will be shown to the respective persons in the form of notification. Using this application seller and buyer can deal without any physical efforts."
-    + "One can easily login anytime, anywhere with correct login id and password and processed for a deal.Seller can search/bid for the product which is "
-    + "added by farmer so transparency about price is maintain during deal and process of online auction become simple.";
-
-
   banners: Banner[] = [
     { image: 'assets/img/krishi-business-dashboard.jpg' },
     { image: 'assets/img/Best-Agricultural-Apps.jpg' },
@@ -29,7 +20,7 @@ export class HomeComponent implements OnInit {
   cards: BannerCard[] = [
     {
       image: 'assets/img/card1.png',
-      title: 'Title: List Your Product',
+      title: 'List Your Product',
       description: 'Easily post the details of your farm product. Set your price and make your products visible to potential buyers instantly'
     }, {
       image: 'assets/img/card2.png',
@@ -41,6 +32,16 @@ export class HomeComponent implements OnInit {
       description: 'Dealer with the highest bid gets selected! Farmers can choose the best offer, ensuring they get the best value for their products'
     }
   ];
+
+  
+  detailArr: string[] = [
+    "\"KRUSHI BUSINESS\" application help to perform online selling and buying of farm goods between farmer, retailer and buyer.",
+    "Application help to create virtual market place for connecting farmer directly with retailers and wholesalers.",
+    "The farmer and buyer can create their profile online where they can update information related with the post of products(like quantity, price, bidding price etc.). The result will be shown to the respective persons in the form of notification. Using this application seller and buyer can deal without any physical efforts.",
+    "One can easily login anytime, anywhere with correct login id and password and processed for a deal. Seller can search/bid for the product which is added by farmer so transparency about price is maintain during deal and process of online auction become simple.",
+  ];
+
+
 
   constructor(
   ) { }
